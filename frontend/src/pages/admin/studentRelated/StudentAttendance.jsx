555dbea0dac102ea25fb8b45d285a567ea67ccb4
Krutik Bundela <krutikbundela2001@gmail.com>
2024-03-19 15:03:56 +0530

    Admin: Student Design

--- frontend/src/pages/admin/studentRelated/StudentAttendance.jsx
+++ frontend/src/pages/admin/studentRelated/StudentAttendance.jsx
@@ -1,100 +1,230 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getUserDetails } from '../../../redux/userRelated/userHandle';
-import { getSubjectList } from '../../../redux/sclassRelated/sclassHandle';
-import { updateStudentFields } from '../../../redux/studentRelated/studentHandle';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getUserDetails } from "../../../redux/userRelated/userHandle";
+import { getSubjectList } from "../../../redux/sclassRelated/sclassHandle";
+import { updateStudentFields } from "../../../redux/studentRelated/studentHandle";
 
 import {
-    Box, InputLabel,
-    MenuItem, Select,
-    Typography, Stack,
-    TextField, CircularProgress, FormControl
-} from '@mui/material';
-import { PurpleButton } from '../../../components/buttonStyles';
-import Popup from '../../../components/Popup';
+  Box,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+  Stack,
+  TextField,
+  CircularProgress,
+  FormControl,
+  Divider,
+} from "@mui/material";
+import { PurpleButton } from "../../../components/buttonStyles";
+import Popup from "../../../components/Popup";
 
 const StudentAttendance = ({ situation }) => {
-    const dispatch = useDispatch();
-    const { currentUser, userDetails, loading } = useSelector((state) => state.user);
-    const { subjectsList } = useSelector((state) => state.sclass);
-    const { response, error, statestatus } = useSelector((state) => state.student);
-    const params = useParams()
+  const dispatch = useDispatch();
+  const { currentUser, userDetails, loading } = useSelector(
+    (state) => state.user
+  );
+  const { subjectsList } = useSelector((state) => state.sclass);
+  const { response, error, statestatus } = useSelector(
+    (state) => state.student
+  );
+  const params = useParams();
 
-    const [studentID, setStudentID] = useState("");
-    const [subjectName, setSubjectName] = useState("");
-    const [chosenSubName, setChosenSubName] = useState("");
-    const [status, setStatus] = useState('');
-    const [date, setDate] = useState('');
+  const [studentID, setStudentID] = useState("");
+  const [subjectName, setSubjectName] = useState("");
+  const [chosenSubName, setChosenSubName] = useState("");
+  const [status, setStatus] = useState("");
+  const [date, setDate] = useState("");
 
-    const [showPopup, setShowPopup] = useState(false);
-    const [message, setMessage] = useState("");
-    const [loader, setLoader] = useState(false)
+  const [showPopup, setShowPopup] = useState(false);
+  const [message, setMessage] = useState("");
+  const [loader, setLoader] = useState(false);
 
-    useEffect(() => {
-        if (situation === "Student") {
-            setStudentID(params.id);
-            const stdID = params.id
-            dispatch(getUserDetails(stdID, "Student"));
-        }
-        else if (situation === "Subject") {
-            const { studentID, subjectID } = params
-            setStudentID(studentID);
-            dispatch(getUserDetails(studentID, "Student"));
-            setChosenSubName(subjectID);
-        }
-    }, [situation]);
-
-    useEffect(() => {
-        if (userDetails && userDetails.sclassName && situation === "Student") {
-            dispatch(getSubjectList(userDetails.sclassName._id, "ClassSubjects"));
-        }
-    }, [dispatch, userDetails]);
-
-    const changeHandler = (event) => {
-        const selectedSubject = subjectsList.find(
-            (subject) => subject.subName === event.target.value
-        );
-        setSubjectName(selectedSubject.subName);
-        setChosenSubName(selectedSubject._id);
+  useEffect(() => {
+    if (situation === "Student") {
+      setStudentID(params.id);
+      const stdID = params.id;
+      dispatch(getUserDetails(stdID, "Student"));
+    } else if (situation === "Subject") {
+      const { studentID, subjectID } = params;
+      setStudentID(studentID);
+      dispatch(getUserDetails(studentID, "Student"));
+      setChosenSubName(subjectID);
     }
+  }, [situation]);
 
-    const fields = { subName: chosenSubName, status, date }
-
-    const submitHandler = (event) => {
-        event.preventDefault()
-        setLoader(true)
-        dispatch(updateStudentFields(studentID, fields, "StudentAttendance"))
+  useEffect(() => {
+    if (userDetails && userDetails.sclassName && situation === "Student") {
+      dispatch(getSubjectList(userDetails.sclassName._id, "ClassSubjects"));
     }
+  }, [dispatch, userDetails]);
 
-    useEffect(() => {
-        if (response) {
-            setLoader(false)
-            setShowPopup(true)
-            setMessage(response)
-        }
-        else if (error) {
-            setLoader(false)
-            setShowPopup(true)
-            setMessage("error")
-        }
-        else if (statestatus === "added") {
-            setLoader(false)
-            setShowPopup(true)
-            setMessage("Done Successfully")
-        }
-    }, [response, statestatus, error])
+  const changeHandler = (event) => {
+    const selectedSubject = subjectsList.find(
+      (subject) => subject.subName === event.target.value
+    );
+    setSubjectName(selectedSubject.subName);
+    setChosenSubName(selectedSubject._id);
+  };
 
-    return (
+  const fields = { subName: chosenSubName, status, date };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setLoader(true);
+    dispatch(updateStudentFields(studentID, fields, "StudentAttendance"));
+  };
+
+  useEffect(() => {
+    if (response) {
+      setLoader(false);
+      setShowPopup(true);
+      setMessage(response);
+    } else if (error) {
+      setLoader(false);
+      setShowPopup(true);
+      setMessage("error");
+    } else if (statestatus === "added") {
+      setLoader(false);
+      setShowPopup(true);
+      setMessage("Done Successfully");
+    }
+  }, [response, statestatus, error]);
+
+  return (
+    <>
+      {loading ? (
         <>
-            {loading
-                ?
-                <>
-                    <div>Loading...</div>
-                </>
-                :
-                <>
-                    <Box
+          <div>Loading...</div>
+        </>
+      ) : (
+        <>
+          <Box
+            sx={{
+              height: "fit-content",
+              width: "100%",
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Box
+              sx={{
+                mt: 1,
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <Typography variant="h2">Student Details</Typography>
+                <Divider
+                  variant="middle"
+                  orientation="horizontal"
+                  sx={{
+                    width: "90%",
+                    borderBottomWidth: "5px",
+                    color: "black",
+                  }}
+                />
+              </Box>
+            </Box>
+            <Stack spacing={1} sx={{ mt: 5, mb: 3 }}>
+              <Typography variant="h4">
+                Student Name: {userDetails.name}
+              </Typography>
+              {currentUser.teachSubject && (
+                <Typography variant="h4">
+                  Subject Name: {currentUser.teachSubject?.subName}
+                </Typography>
+              )}
+            </Stack>
+            <form onSubmit={submitHandler} style={{ width: "40%" }}>
+              <Stack spacing={3}>
+                {situation === "Student" && (
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      Select Subject
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={subjectName}
+                      label="Choose an option"
+                      onChange={changeHandler}
+                      required
+                    >
+                      {subjectsList ? (
+                        subjectsList.map((subject, index) => (
+                          <MenuItem key={index} value={subject.subName}>
+                            {subject.subName}
+                          </MenuItem>
+                        ))
+                      ) : (
+                        <MenuItem value="Select Subject">
+                          Add Subjects For Attendance
+                        </MenuItem>
+                      )}
+                    </Select>
+                  </FormControl>
+                )}
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    Attendance Status
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={status}
+                    label="Choose an option"
+                    onChange={(event) => setStatus(event.target.value)}
+                    required
+                  >
+                    <MenuItem value="Present">Present</MenuItem>
+                    <MenuItem value="Absent">Absent</MenuItem>
+                  </Select>
+                </FormControl>
+                <FormControl>
+                  <TextField
+                    label="Select Date"
+                    type="date"
+                    value={date}
+                    onChange={(event) => setDate(event.target.value)}
+                    required
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </FormControl>
+              </Stack>
+
+              <PurpleButton
+                fullWidth
+                size="large"
+                sx={{ mt: 3 }}
+                variant="contained"
+                type="submit"
+                disabled={loader}
+              >
+                {loader ? (
+                  <CircularProgress size={24} color="inherit" />
+                ) : (
+                  "Submit"
+                )}
+              </PurpleButton>
+            </form>
+            {/* <Box
                         sx={{
                             flex: '1 1 auto',
                             alignItems: 'center',
@@ -109,89 +239,19 @@ const StudentAttendance = ({ situation }) => {
                                 py: '100px',
                                 width: '100%'
                             }}
-                        >
-                            <Stack spacing={1} sx={{ mb: 3 }}>
-                                <Typography variant="h4">
-                                    Student Name: {userDetails.name}
-                                </Typography>
-                                {currentUser.teachSubject &&
-                                    <Typography variant="h4">
-                                        Subject Name: {currentUser.teachSubject?.subName}
-                                    </Typography>
-                                }
-                            </Stack>
-                            <form onSubmit={submitHandler}>
-                                <Stack spacing={3}>
-                                    {
-                                        situation === "Student" &&
-                                        <FormControl fullWidth>
-                                            <InputLabel id="demo-simple-select-label">Select Subject</InputLabel>
-                                            <Select
-                                                labelId="demo-simple-select-label"
-                                                id="demo-simple-select"
-                                                value={subjectName}
-                                                label="Choose an option"
-                                                onChange={changeHandler} required
-                                            >
-                                                {subjectsList ?
-                                                    subjectsList.map((subject, index) => (
-                                                        <MenuItem key={index} value={subject.subName}>
-                                                            {subject.subName}
-                                                        </MenuItem>
-                                                    ))
-                                                    :
-                                                    <MenuItem value="Select Subject">
-                                                        Add Subjects For Attendance
-                                                    </MenuItem>
-                                                }
-                                            </Select>
-                                        </FormControl>
-                                    }
-                                    <FormControl fullWidth>
-                                        <InputLabel id="demo-simple-select-label">Attendance Status</InputLabel>
-                                        <Select
-                                            labelId="demo-simple-select-label"
-                                            id="demo-simple-select"
-                                            value={status}
-                                            label="Choose an option"
-                                            onChange={(event) => setStatus(event.target.value)}
-                                            required
-                                        >
-                                            <MenuItem value="Present">Present</MenuItem>
-                                            <MenuItem value="Absent">Absent</MenuItem>
-                                        </Select>
-                                    </FormControl>
-                                    <FormControl>
-                                        <TextField
-                                            label="Select Date"
-                                            type="date"
-                                            value={date}
-                                            onChange={(event) => setDate(event.target.value)} required
-                                            InputLabelProps={{
-                                                shrink: true,
-                                            }}
-                                        />
-                                    </FormControl>
-                                </Stack>
+                        > */}
 
-                                <PurpleButton
-                                    fullWidth
-                                    size="large"
-                                    sx={{ mt: 3 }}
-                                    variant="contained"
-                                    type="submit"
-                                    disabled={loader}
-                                >
-                                    {loader ? <CircularProgress size={24} color="inherit" /> : "Submit"}
-                                </PurpleButton>
-                            </form>
-                        </Box>
-                    </Box>
-                    <Popup message={message} setShowPopup={setShowPopup} showPopup={showPopup} />
-                </>
-            }
+            {/* </Box> */}
+          </Box>
+          <Popup
+            message={message}
+            setShowPopup={setShowPopup}
+            showPopup={showPopup}
+          />
         </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default StudentAttendance
+export default StudentAttendance;
