971189f5867578a6685696dcf4e29b9b1ad9d717
Krutik Bundela <krutikbundela2001@gmail.com>
2024-04-02 13:22:22 +0530

    Teacher: home Page

--- frontend/src/pages/teacher/TeacherHomePage.jsx
+++ frontend/src/pages/teacher/TeacherHomePage.jsx
@@ -1,81 +1,84 @@
-import { Container, Grid, Paper } from '@mui/material'
-import SeeNotice from '../../components/SeeNotice';
-import CountUp from 'react-countup';
-import styled from 'styled-components';
+import { Container, Grid, Paper, Box } from "@mui/material";
+import SeeNotice from "../../components/SeeNotice";
+import CountUp from "react-countup";
+import styled from "styled-components";
 import Students from "../../assets/img1.png";
 import Lessons from "../../assets/subjects.svg";
 import Tests from "../../assets/assignment.svg";
 import Time from "../../assets/time.svg";
-import { getClassStudents, getSubjectDetails } from '../../redux/sclassRelated/sclassHandle';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import {
+  getClassStudents,
+  getSubjectDetails,
+} from "../../redux/sclassRelated/sclassHandle";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const TeacherHomePage = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const { currentUser } = useSelector((state) => state.user);
-    const { subjectDetails, sclassStudents } = useSelector((state) => state.sclass);
+  const { currentUser } = useSelector((state) => state.user);
+  const { subjectDetails, sclassStudents } = useSelector(
+    (state) => state.sclass
+  );
 
-    const classID = currentUser.teachSclass?._id
-    const subjectID = currentUser.teachSubject?._id
+  const classID = currentUser.teachSclass?._id;
+  const subjectID = currentUser.teachSubject?._id;
 
-    useEffect(() => {
-        dispatch(getSubjectDetails(subjectID, "Subject"));
-        dispatch(getClassStudents(classID));
-    }, [dispatch, subjectID, classID]);
+  useEffect(() => {
+    dispatch(getSubjectDetails(subjectID, "Subject"));
+    dispatch(getClassStudents(classID));
+  }, [dispatch, subjectID, classID]);
 
-    const numberOfStudents = sclassStudents && sclassStudents.length;
-    const numberOfSessions = subjectDetails && subjectDetails.sessions
+  const numberOfStudents = sclassStudents && sclassStudents.length;
+  const numberOfSessions = subjectDetails && subjectDetails.sessions;
 
-    return (
-        <>
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <StyledPaper>
-                            <img src={Students} alt="Students" />
-                            <Title>
-                                Class Students
-                            </Title>
-                            <Data start={0} end={numberOfStudents} duration={2.5} />
-                        </StyledPaper>
-                    </Grid>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <StyledPaper>
-                            <img src={Lessons} alt="Lessons" />
-                            <Title>
-                                Total Lessons
-                            </Title>
-                            <Data start={0} end={numberOfSessions} duration={5} />
-                        </StyledPaper>
-                    </Grid>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <StyledPaper>
-                            <img src={Tests} alt="Tests" />
-                            <Title>
-                                Tests Taken
-                            </Title>
-                            <Data start={0} end={24} duration={4} />
-                        </StyledPaper>
-                    </Grid>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <StyledPaper>
-                            <img src={Time} alt="Time" />
-                            <Title>
-                                Total Hours
-                            </Title>
-                            <Data start={0} end={30} duration={4} suffix="hrs"/>                        </StyledPaper>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                            <SeeNotice />
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <Box
+        sx={{
+          height: "fit-content",
+          width: "100%",
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          flexDirection: "column",
+        }}
+      >
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Grid
+            container
+            spacing={3}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Grid item xs={12} md={3} lg={3}>
+              <StyledPaper>
+                <img src={Students} alt="Students" />
+                <Title>Class Students</Title>
+                <Data start={0} end={numberOfStudents} duration={2.5} />
+              </StyledPaper>
+            </Grid>
+            <Grid item xs={12} md={3} lg={3}>
+              <StyledPaper>
+                <img src={Lessons} alt="Lessons" />
+                <Title>Total Lectures</Title>
+                <Data start={0} end={numberOfSessions} duration={5} />
+              </StyledPaper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                <SeeNotice />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 const StyledPaper = styled(Paper)`
   padding: 16px;
@@ -92,8 +95,8 @@ const Title = styled.p`
 `;
 
 const Data = styled(CountUp)`
-  font-size: calc(1.3rem + .6vw);
+  font-size: calc(1.3rem + 0.6vw);
   color: green;
 `;
 
-export default TeacherHomePage
+export default TeacherHomePage;
